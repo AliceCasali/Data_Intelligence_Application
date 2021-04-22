116d33b585ee2d5dab700fb58b25e7db20088ee8
famorosini <oauth2:JzVGPPWxkNsRuQBBhpdy>
2021-04-22 10:31:14 +0200

    first step with env

--- environment.py
+++ environment.py
@@ -1,40 +1,36 @@
-  
 import numpy as np
 import statistics
 from scipy.stats import truncnorm
 
-
-
 class Environment():
-    '''
-        gaussian_parameters should be a list of lists of the form:
-            gaussian_parameters [0] : mean of class i
-            gaussian_parameters [1] : variance of class i
-    '''
-    def __init__(self, gaussian_parameters,discounts):#,conversion_rate):
+    def __init__(self, gaussian_parameters, discounts, prices, conv_rate1, conv_rate2):
         #self.probabilities = probabilities
-        self.mean = gaussian_parameters[0]
-        self.variances = gaussian_parameters[1]
+        means, variances = zip(*gaussian_parameters)
+        means = np.array(means)
+        variances = np.array(variances)
 
         myclip_a = 0
-        myclip_b = 200
-        self.class_customer_distribution = []
-        
-        for i in range(0,4):
-            a, b = (myclip_a - gaussian_parameters[0][i]) / gaussian_parameters[1][i], (myclip_b - gaussian_parameters[0][i]) / gaussian_parameters[1][i]
-            self.class_customer_distribution.append(truncnorm(a,b))
-        
-        '''self.conversion_rate_0 = conversion_rate[0]
-        self.conversion_rate_1 = conversion_rate[1]
-        self.conversion_rate_2 = conversion_rate[2]
-        self.conversion_rate_3 = conversion_rate[3] '''    
-    
-        self.promo0 = 0
-        self.promo1 = discounts[0]
-        self.promo2 = discounts[1]
-        self.promo3 = discounts[2]
+        myclip_b = 50
+        a,b = (myclip_a - means)/variances, (myclip_b-means)/variances
 
-'''    def update_probabilities(self,optimal_solution):
+        self.class_customer_distribution = [truncnorm(alpha,beta) for alpha,beta in zip(a,b)]
+        self.discounts = discounts
+        self.prices = prices
+        self.conv1 = conv_rate1
+        self.conv2 = conv_rate2
+        self.nclasses = len(means)
+
+    def calculation_probabilities_for_update(self,fractions,graph, class_id, MODE):
+        
+        p = [ 0 for x in range(0,4)]
+        index_max = np.argmax(graph[class_id][0])
+        p[index_max] = graph[class_id][0][index_max] #Not so sure about this syntax...
+        indices = [x for x in range(0,4) if x!= index_max]
+        for i in indices:
+            p[i] = graph[class_id][0][i]
+        return p    
+
+    ''' def update_probabilities(self,optimal_solution):
         #given a optimal solution, it returns an array for each discount
         #for example: in optimal solution c0 is assigned to p0, it will be:
         #p0=[0.75 0.15 0.05 0.05]
@@ -53,32 +49,20 @@ class Environment():
         p3 = self.calculation_probabilities_for_update(best_assignment_3)
 
         return p0,p1,p2,p3'''
-    
-    def calculation_probabilities_for_update(self,fractions,graph, class_id, MODE):
-        
-        p = [ 0 for x in range(0,4)]
-        if MODE == 'no_fraction':
-            p[best_assignment] = 0.70
-            indices = [x for x in range(0,4) if x!= best_assignment]
-            for i in indices:
-                p[i] = 0.10
-            return p
-        elif MODE == 'fraction':
-            index_max = np.argmax(graph[class_id][0])
-            p[index_max] = graph[class_id][0][index_max] #Not so sure about this syntax...
-            indices = [x for x in range(0,4) if x!= index_max]
-            for i in indices:
-                p[i] = graph[class_id][0][i]
-            return p
 
 
-    
-gaussian_parameters = [1,2,3,4],[3,2,1,2]
-promo_codes = [0.25, 0.30, 0.50, 0.75]
-env = Environment(gaussian_parameters, promo_codes)
-optimal_solution = [0,2,3,1]
-p0,p1,p2,p3 = env.update_probabilities(optimal_solution)
-print(p0)
-print(p1)
-print(p2)
-print(p3)
+#Customers
+customerParams = zip([10,10,10,10], [1,1,1,1])
+#discounts
+discounts = np.array([0.0, 0.5, 0.10, 0.25])
+#Prices
+prices = np.array([250, 400])
+#Conversion Rates for item 1
+conv_rate1 = [0.3, 0.4, 0.25, 0.45]
+#Conversion Rates for item 2
+conv_rate2 = np.array([[0.1, 0.2, 0.15, 0.2],#p0
+                    [0.15, 0.25, 0.2, 0.25],#p1
+                    [0.2, 0.35, 0.25, 0.4],#p2
+                    [0.4, 0.45, 0.35, 0.6]])#p3
+
+env = Environment(customerParams, discounts, prices, conv_rate1, conv_rate2)
