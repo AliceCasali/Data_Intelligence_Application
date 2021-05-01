c04e8d2d48cf6cdb61eeb6762050bb1214563e7e
Anna Giovannacci <49471830+annagiovannacci@users.noreply.github.com>
2021-05-01 17:59:35 +0200

    Third step
    siamo fortissime
    eng version
    we're best coders in whole world

--- mab/UCB_Learner.py
+++ mab/UCB_Learner.py
@@ -2,10 +2,10 @@ from learner import Learner
 import numpy as np
 
 class UCB(Learner):
-    def __init__(self, n_arms):
-        super().__init__(n_arms)
-        self.empirical_means = np.zeros(n_arms)
-        self.confidence = np.array([np.inf]*n_arms)
+    def __init__(self, arms):
+        super().__init__(arms)
+        self.empirical_means = np.zeros(self.n_arms)
+        self.confidence = np.array([np.inf]*self.n_arms)
 
     def pull_arm(self):
         upper_conf = self.empirical_means + self.confidence
@@ -17,6 +17,6 @@ class UCB(Learner):
         for a in range(self.n_arms):
             n_samples = len(self.rewards_per_arm[a])
             self.confidence[a] = (2*np.log(self.t)/n_samples)**0.5 if n_samples > 0 else np.inf
-        #self.update_observations(pull_arm, reward)
+        self.update_observations(pull_arm, reward)
 
 
--- mab/comparison.py
+++ mab/comparison.py
@@ -1,38 +1,55 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from environment import *
-from greedy_learner import *
+from ts_learner import *
+from UCB_Learner import *
 
-p = np.array([ 0.15, 0.1, 0.1, 0.35])
+
+
 prices = [500, 690, 750, 850]
-opt = p[3]
+
+
+
+def generate_conversion_rate(prices):
+    val = np.random.uniform(size=(len(prices)))
+    conversion_rates = np.sort(val)[::-1]
+    return conversion_rates
 
 T = 300
 
+conversion_rate_first_item = generate_conversion_rate(prices)
+n_customers =  1000
+ 
+max_conv = np.argmax(conversion_rate_first_item)
+
+opt = conversion_rate_first_item[max_conv]
+print(opt)
+
 n_experiments = 200
 ts_rewards_per_experiment = []
-gr_rewards_per_experiment = []
+u_rewards_per_experiment = []
 
 for e in range(0, n_experiments):
-    env = Environment(n_arms=len(prices), probabilities = p)
-    ts_learner = TS_Learner(arms=prices)
-    gr_learner = Gready_Learner(arms=prices)
+    env = Environment(n_arms=len(prices), probabilities = conversion_rate_first_item)
+    ts_learner =TS_Learner(arms=prices)
+    u_learner = UCB(arms=prices)
     for t in range(0,T):
         pulled_arm = ts_learner.pull_arm()
         reward = env.round(pulled_arm)
         ts_learner.update(pulled_arm,reward)
 
-        pulled_arm = gr_learner.pull_arm()
+        pulled_arm = u_learner.pull_arm()
         reward = env.round(pulled_arm)
-        gr_learner.update(pulled_arm,reward)
+        u_learner.update(pulled_arm,reward)
 
     ts_rewards_per_experiment.append(ts_learner.rewards)
-    gr_rewards_per_experiment.append(gr_learner.rewards)
+    u_rewards_per_experiment.append(u_learner.rewards)
+
 
 plt.figure(0)
 plt.xlabel("t")
 plt.ylabel("Regret")
 plt.plot(np.cumsum(np.mean(opt - ts_rewards_per_experiment, axis=0)), 'r')
-plt.plot(np.cumsum(np.mean(opt - gr_rewards_per_experiment, axis=0)), 'g')
-plt.legend(["TS", "Greedy"])
+plt.plot(np.cumsum(np.mean(opt - u_rewards_per_experiment, axis=0)), 'g')
+plt.legend(["TS", "UCB"])
 plt.show()
