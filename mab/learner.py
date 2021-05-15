046679eb41b2ce12e1297839ab9800d27964b047
famorosini <oauth2:JzVGPPWxkNsRuQBBhpdy>
2021-05-15 21:50:15 +0200

    Refactor with step3 for classes

--- mab/learner.py
+++ mab/learner.py
@@ -2,15 +2,15 @@ import numpy as np
 
 class Learner():
     # Generale Learner Class
-    def __init__(self, arms):
-        self.n_arms = len(arms)
-        self.arms = arms
+    def __init__(self, n_arms, n_classes=4):
+        self.n_arms = n_arms
+        self.n_classes = n_classes
         self.t = 0
-        self.rewards = []
-        self.rewards_per_arm = [[] for i in range(self.n_arms)]
-        self.pulled_arms = []
+        self.collected_rewards = [[]for i in range(n_classes)]
+        self.pulled_arms = [[] for i in range(n_classes)]
+        self.rewards_per_arm = np.array([np.zeros(self.n_arms) for i in range(n_classes)])
 
-    def update_observations(self, pulled_arm, reward):
-        self.rewards_per_arm[pulled_arm].append(reward)
-        self.pulled_arms.append(pulled_arm)
-        self.rewards.append(reward)
+    def update_observations(self, pulled_arms, reward, c): 
+        self.rewards_per_arm[c, pulled_arms[c]] = reward[c]
+        self.pulled_arms[c].append(pulled_arms[c])
+        self.collected_rewards[c].append(reward[c])
--- mab/ts_learner.py
+++ mab/ts_learner.py
@@ -1,18 +1,32 @@
-from learner import *
+from mab.learner import *
+import numpy as np
 
 
 class TS_Learner(Learner):
-    # Specialized Thompson Sampling Learner
-    def __init__(self, arms):
-        super().__init__(arms=arms)
-        self.beta_param = np.ones((self.n_arms, 2))
+  # Specialized Thompson Sampling Learner
+  def __init__(self, arms,n_classes = 4):
+    super().__init__(n_arms=len(arms),n_classes=n_classes)
+    self.arms = np.array(arms)
+    self.beta_parameters = np.array([np.ones([len(arms), 2]) for x in range(n_classes)])
 
-    def pull_arm(self):
-        idx = np.argmax(np.array(self.arms) * np.random.beta(self.beta_param[:, 0], self.beta_param[:, 1]))
-        return idx
+  def pull_arm_per_class(self):
+    idx = []
+    for i in range(self.n_classes):
+      idx.append(np.argmax(self.arms * np.random.beta(self.beta_parameters[i][:, 0], self.beta_parameters[i][:, 1])))
+    return idx
+  
+  def select_fractions(self):
+    fractions_idxs = []
+    for i in range(4):
+      idx = np.argmax(np.random.beta(self.beta_parameters[i][:,0], self.beta_parameters[i][:,1]))
+      fractions_idxs.append(idx)
+    return fractions_idxs
 
-    def update(self, pulled_arm, reward):
-        self.update_observations(pulled_arm, reward)
-        self.beta_param[pulled_arm, 0] = self.beta_param[pulled_arm, 0] + reward
-        self.beta_param[pulled_arm, 1] = self.beta_param[pulled_arm, 1] + 1.0 - reward
-        self.t += 1
+  def update(self, pulled_arms, reward, to_be_updated):
+    #print(pulled_arm)
+    for c in range(self.n_classes):
+      if not to_be_updated[c] : continue
+      self.update_observations(pulled_arms, reward, c)
+      self.beta_parameters[c][pulled_arms[c], 0] = self.beta_parameters[c][pulled_arms[c], 0] + reward[c]
+      self.beta_parameters[c][pulled_arms[c], 1] = self.beta_parameters[c][pulled_arms[c], 1] + 1 - reward[c] #TODO: this was originally 1.0, we need some refactor to include such scenario
+    self.t += 1
