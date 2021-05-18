ba9e6ce288774ca5ccae34dcd7a47e8bf61b46bb
famorosini <oauth2:JzVGPPWxkNsRuQBBhpdy>
2021-05-18 21:48:28 +0200

    step 3 refactored and almost working

--- mab/ts_learner.py
+++ mab/ts_learner.py
@@ -7,18 +7,18 @@ class TS_Learner(Learner):
   def __init__(self, arms,n_classes = 4):
     super().__init__(n_arms=len(arms),n_classes=n_classes)
     self.arms = np.array(arms)
-    self.beta_parameters = np.array([np.ones([len(arms), 2]) for x in range(n_classes)])
+    self.beta_parameters = np.array([np.ones(2) for x in range(len(arms))])
 
   def pull_arm_per_class(self):
     idx = []
     for i in range(self.n_classes):
-      idx.append(np.argmax(self.arms * np.random.beta(self.beta_parameters[i][:, 0], self.beta_parameters[i][:, 1])))
+      idx.append(np.argmax(self.arms * np.random.beta(self.beta_parameters[:, 0], self.beta_parameters[:, 1])))
     return idx
   
   def select_fractions(self):
     fractions_idxs = []
     for i in range(self.n_classes):
-      idx = np.argmax(np.random.beta(self.beta_parameters[i][:,0], self.beta_parameters[i][:,1]))
+      idx = np.argmax(np.random.beta(self.beta_parameters[:,0], self.beta_parameters[:,1]))
       fractions_idxs.append(idx)
     return fractions_idxs
 
@@ -27,6 +27,6 @@ class TS_Learner(Learner):
     for c in range(self.n_classes):
       if not to_be_updated[c] : continue
       self.update_observations(pulled_arms, reward, c)
-      self.beta_parameters[c][pulled_arms[c], 0] = self.beta_parameters[c][pulled_arms[c], 0] + reward[c]
-      self.beta_parameters[c][pulled_arms[c], 1] = self.beta_parameters[c][pulled_arms[c], 1] + 1 - reward[c] #TODO: this was originally 1.0, we need some refactor to include such scenario
+      self.beta_parameters[pulled_arms[c], 0] = self.beta_parameters[pulled_arms[c], 0] + reward[c]
+      self.beta_parameters[pulled_arms[c], 1] = self.beta_parameters[pulled_arms[c], 1] + 1 - reward[c] #TODO: this was originally 1.0, we need some refactor to include such scenario
     self.t += 1
--- mab/ucb_learner.py
+++ mab/ucb_learner.py
@@ -5,21 +5,21 @@ import numpy as np
 class UCB(Learner):
     def __init__(self, n_arms,n_classes = 4):
         super().__init__(n_arms,n_classes = n_classes)
-        self.empirical_means = np.zeros([n_classes, n_arms])
-        self.confidence = np.ones([n_classes,n_arms])*np.inf
+        self.empirical_means = np.zeros([n_arms])
+        self.confidence = np.ones([n_arms])*np.inf
 
     def pull_arm_per_class(self):
         uppers = []
         for i in range(self.n_classes):
-            uppers.append(self.empirical_means[i,:] + self.confidence[i,:])
+            uppers.append(self.empirical_means[:] + self.confidence[:])
         return [np.random.choice(np.where(x == x.max())[0]) for x in uppers]
 
     def update(self, pulled_arms, reward, to_be_updated):
         self.t += 1
         for c in range(self.n_classes):
             if not to_be_updated[c] : continue
-            self.empirical_means[c][pulled_arms[c]] = (self.empirical_means[c][pulled_arms[c]]*(self.t-1) + reward[c])/self.t
+            self.empirical_means[pulled_arms[c]] = (self.empirical_means[pulled_arms[c]]*(self.t-1) + reward[c])/self.t
             for a in range(self.n_arms):
                 n_samples = len(self.rewards_per_arm[c])
-                self.confidence[c][a] = (2*np.log(self.t)/n_samples)**0.5 if n_samples > 0 else np.inf
+                self.confidence[a] = (2*np.log(self.t)/n_samples)**0.5 if n_samples > 0 else np.inf
             self.update_observations(pulled_arms, reward, c)
