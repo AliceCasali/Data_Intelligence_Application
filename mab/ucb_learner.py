5761cf2d8c40bf98be114fed2574c96abb42975f
famorosini <oauth2:JzVGPPWxkNsRuQBBhpdy>
2021-08-19 16:54:26 +0200

    fixed ucb

--- mab/ucb_learner.py
+++ mab/ucb_learner.py
@@ -16,7 +16,7 @@ class UCB(Learner):
     def update(self, pull_arm,reward,cust_class):
         self.t += 1
         self.empirical_means[cust_class][pull_arm] = (self.empirical_means[cust_class][pull_arm]*(self.t-1) + reward)/self.t
-        for a in range(self.n_arms):
-            n_samples = len(self.rewards_per_arm[cust_class])
-            self.confidence[cust_class][a] = (2*np.log(self.t)/n_samples)**0.5 if n_samples > 0 else np.inf
+        
+        n_samples = len(self.rewards_per_arm[cust_class])
+        self.confidence[cust_class][pull_arm] = (2*np.log(self.t)/n_samples)**0.5 if n_samples > 0 else np.inf
         self.update_observations(pull_arm, reward,cust_class)
